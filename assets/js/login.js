5038dafc493642c680e879cc9111b4be29ca536b
小羽-lyh <9331721+liu-yuhui163@user.noreply.gitee.com>
2022-01-05 16:38:28 +0800

    完成了注册和登录功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,18 +14,18 @@ $(function() {
   // 从 layui 中获取 form 对象
   var form = layui.form
   var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+    // 通过 form.verify() 函数自定义校验规则
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
     // 校验两次密码是否一致的规则
-    repwd: function(value) {
+    repassword: function(value) {
       // 通过形参拿到的是确认密码框中的内容
       // 还需要拿到密码框中的内容
       // 然后进行一次等于的判断
       // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
+      var password = $('.reg-box [name=password]').val()
+      if (password !== value) {
         return '两次密码不一致！'
       }
     }
@@ -35,7 +35,7 @@ $(function() {
   $('#form_reg').on('submit', function(e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
-    // 2. 发起Ajax的POST请求
+      // 2. 发起Ajax的POST请求
     var data = {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
@@ -45,7 +45,7 @@ $(function() {
         return layer.msg(res.message)
       }
       layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
+        // 模拟人的点击行为
       $('#link_login').click()
     })
   })
@@ -64,11 +64,139 @@ $(function() {
           return layer.msg('登录失败！')
         }
         layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+          // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem('token', res.token)
-        // 跳转到后台主页
+          // 跳转到后台主页
         location.href = '/index.html'
       }
     })
   })
 })
+
+
+// $(function() {
+//   $('#link_reg').on('click', function() {
+//     $('.login-box').hide()
+//     $('.reg-box').show()
+//   })
+//   $('#link_login').on('click', function() {
+//     $('.login-box').show()
+//     $('.reg-box').hide()
+//   })
+//   var form = layui.form
+//   var layer = layui.layer
+//   form.verify({
+//     password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+//     repassword: function(value) {
+//       var password = $('.reg-box [name=password]').val()
+//       if (password !== value) {
+//         return '两次密码不一致！'
+//       }
+//     }
+//   })
+//   $('#form_reg').on('submit', function(e) {
+//     e.preventDefault()
+//     var data = {
+//       username: $('#form_reg [name=username]').val(),
+//       password: $('#form_reg [name=password]').val()
+//     }
+//     $.post('/api/reguser', data, function(res) {
+//       if (res.status !== 0) {
+//         return layer.msg(res.message)
+//       }
+//       layer.msg('注册成功，请登录！')
+//       $('#link_login').click()
+//     })
+//   })
+//   $('#form_login').submit(function(e) {
+//     e.preventDefault()
+//     $.ajax({
+//       url: '/api/login',
+//       method: 'POST',
+//       data: $(this).serialize(),
+//       success: function(res) {
+//         if (res.status !== 0) {
+//           return layer.msg('登录失败！')
+//         }
+//         layer.msg('登录成功！')
+//         localStorage.setItem('token', res.token)
+//         location.href = '/index.html'
+//       }
+//     })
+//   })
+// })
+
+
+
+
+
+// $(function() {
+//   // 点击“去注册账号”
+//   $('#link_reg').on('click', function() {
+//     $('.login-box').hide()
+//     $('.reg-box').show()
+//   })
+
+//   // 点击“去登录”
+//   $('#link_login').on('click', function() {
+//     $('.login-box').show()
+//     $('.reg-box').hide()
+//   })
+
+//   // 从 layui 中获取 form 对象
+//   var form = layui.form
+//   var layer = layui.layer
+//     // 通过 form.verify() 函数自定义校验规则
+//   form.verify({
+//     // 自定义一个叫 pwd 校验规则
+//     password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+//     // 校验两次密码是否相同规则
+//     repassword: function(value) {
+//       var password = $('.reg-box [name=password]').val()
+//       if (password !== value) {
+//         return '两次密码不一致！'
+//       }
+//     }
+//   })
+
+
+//   // 监听注册表单的提交事件
+//   $('#form_reg').on('sumbit', function(e) {
+//     // 阻止默认行为
+//     e.preventDefault();
+//     var data = {
+//       username: $('#form_reg [name=username]').val(),
+//       password: $('#form_reg [name=password]').val()
+//     }
+//     $.post('/api/reguser', data, function(res) {
+//       if (res.status !== 0) {
+//         return layer.msg(res.message)
+//       }
+//       layer.msg('注册成功');
+//       // 模拟人的点击行为
+//       $('#link_login').click()
+//     })
+//   })
+
+
+//   // 监听登录表单的提交事件
+//   $('#form_login').submit(function(e) {
+//     e.preventDefault();
+//     $.ajax({
+//       url: '/api/login',
+//       method: 'POST',
+//       data: $(this).serialize(),
+//       success: function(res) {
+//         if (res.status !== 0) {
+//           return layer.msg(res.message)
+//         }
+//         layer.msg('登录成功！')
+//         console.log(res);
+//         // 将登录成功得到的 token，保存到 localStorage
+//         localStorage.setItem('token', res.token)
+//           // 跳转页面
+//         location.href = '/index.html'
+//       }
+//     })
+//   })
+// })
